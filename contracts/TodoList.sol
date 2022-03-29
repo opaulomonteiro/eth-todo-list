a9131754a5d6c6bb663f4fbb33903e69fe962e28
opaulomonteiro <paulo_vicente01@hotmail.com>
2022-03-29 18:22:23 -0300

    List task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Check out test");
+  }
+
+  function createTask(string memory _content) public{
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
